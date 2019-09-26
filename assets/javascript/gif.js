1e8888b684cd4a58429cd39ab92eb1ee18a50148
JC Lenz <jc@miserablepop.com>
2019-09-26 14:07:43 -0700

    Add for loop to create buttons based off values in the topics array.

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
 
     // VARIABLES
-    ///////////////////////////////////////////////////////////////////////////////
-
+    ////////////////////////////////////////////////////////////////////////////////
+    
+    // Array to store topics to generate buttons
     var topics = ['Messi', "Cristano Ronaldo", "Robert Lewandowski",'Eden Hazard', 'Neymar', 'Raheem Sterling',
                     'Kevin de Bruyne', 'Kylian Mbappe', 'Mohamed Salah', 'Virgil van Dijk'];
 
     
+    // FUNCTIONS
+    ////////////////////////////////////////////////////////////////////////////////
+
+    function renderButtons(topic){
+        
+        // Empty div for the generate buttons
+        $('#buttons-view').empty();
+        
+        for (var i = 0; i < topics.length; i++){
+            $('#buttons-view').append('<button>' + topics[i] + '</button>');
+        }
+
+    }
+
+
+
+    renderButtons();
+
 });
