06642976211ba9bd2df53f72de0cd61a37479090
JC Lenz <jc@miserablepop.com>
2019-09-26 13:56:58 -0700

    Updated index.html file with input form and submit button. Updated gif.js with topics array

---
+++ assets/javascript/gif.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // VARIABLES
+    ///////////////////////////////////////////////////////////////////////////////
+
+    var topics = ['Messi', "Cristano Ronaldo", "Robert Lewandowski",'Eden Hazard', 'Neymar', 'Raheem Sterling',
+                    'Kevin de Bruyne', 'Kylian Mbappe', 'Mohamed Salah', 'Virgil van Dijk'];
+
+    
+});
